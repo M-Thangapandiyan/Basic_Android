c1818557b2a138873f11fe267fedcfe3cd203d86
Thangapandiyan <thangapandiyan.muruganponnaiya@ideas2it.com>
2023-11-24 18:47:47 +0530

    kotlin class, singlelevel inheritance, higher order function

--- java.js/src/Demo/BasicsKotlin.kt
+++ java.js/src/Demo/BasicsKotlin.kt
@@ -1,5 +1,7 @@
 package Demo
 
+import kotlin.reflect.KProperty0
+
 //import java.util.Random
 //
 //class Array{
@@ -248,9 +250,164 @@ package Demo
 
 //example for get and set function
 
-fun main(){
-    val s = VolumeCalculator(20,20,30)
-    println(s.volume)
-    println(s.a)
+//fun main(){
+//    val s = VolumeCalculator(20,20,30)
+//    println(s.volume)
+//    println(s.a)
+//}
+
+
+//const val a = 10
+//
+//const val b =11
+//fun  main(){
+//    var c = a+ b
+//    println(c)
+//}
+
+//public class A {
+//     fun someFunction() {
+//        println("Function in class A")
+//    }
+//}
+//
+//class B : A() {
+//    fun someFunction1() {
+//        println("Function in class B")
+//    }
+//}
+
+
+
+
+//data class Person(val name: String, val age: Int)
+//
+//fun main() {
+//    val person1 = Person("Alice", 30)
+//    val person2 = Person("Alice", 30)
+//    val person3 = Person("Bob", 25)
+//
+//    println("person1 == person2: ${person1.equals(person2)}")
+//    println("person1 == person3: ${person1.equals(person3)}")
+//
+//    println("HashCode of person1: ${person1.hashCode()}")
+//    println("HashCode of person2: ${person2.hashCode()}")
+//    println("HashCode of person3: ${person3.hashCode()}")
+//    println("Custom toString(): ${person1.age.toString()}") // Explicit call to overridden toString()
+//
+//    val(name,age) = person2
+//
+//    println("name" + name + "age" + age)
+//    println("name = $name and age = $age")
+//
+//    val person5 = Person("Alice", 30)
+//
+//    val person6 = person1.copy(name = "j",age = 25)
+//    println("Original Person: $person5")
+//    println("Copied Person with modified age: $person6")
+//}
+
+//data class Book(
+//        val title: String,
+//        val author: String,
+//        val genres: ArrayList<String>
+//)
+//
+//fun main() {
+//
+//    val bookGenres = arrayListOf("Fiction", "Mystery", "Thriller")
+//
+//    val book = Book("The Da Vinci Code", "Dan Brown", bookGenres)
+//
+//    val (title, author, genres) = book
+//
+//    println("Book Title: $title")
+//    println("Author: $author")
+//    println("Genres: ${genres.joinToString(", ")}")
+//
+//    genres.add("Adventure")
+//
+//    println("\nModified Genres: ${genres.joinToString(", ")}")
+//}
+
+
+
+//sealed class Number{
+//    class Number1 (val num1: Int) : Number()
+//    class Number2(val num2 : Int) : Number()
+//}
+//
+//fun demo(number: Number) {
+//    when(number){
+//        is Number.Number1 -> println("this is ${number.num1}")
+//        is Number.Number2-> println("this is number2")
+//    }
+//}
+//
+//fun main() {
+//    val n = Number.Number1(2)
+//    val n1= Number.Number2(4)
+//    demo(n)
+//    demo(n1)
+//}
+
+
+
+// Example for ComponentN()
+
+
+//data class Number (val num1 : Int, val num2:Int, val num4: java.util.ArrayList<Int>)
+//
+//fun main(){
+//
+//    val num4 = arrayListOf(11,2,1)
+//
+//    val a = Number(2,2, num4)
+//
+//   val(num1,num2, num3) = a
+//
+//    println(num3)
+//}
+
+
+
+//val addition: (Int, Int) -> Int = { a, b -> a + b }
+//
+//fun higherOrder(addition: (Int, Int) -> Int): (Int, Int) -> Int {
+//    return ::multiply
+//}
+//
+//fun multiply(): (Int, Int) -> Int = { a, b -> a * b }
+//
+//fun main() {
+//    val result = higherOrder(addition)(3, 4)
+//    println("Result of multiplication: $result")
+//}
+
+
+//val addition: (Int, Int) -> Int = { a, b -> a + b }
+//
+//fun higherOrder(addition: (Int, Int) -> Int): (Int, Int) -> Int {
+//    return { x, y -> addition(x, y) * 2 }
+//}
+//
+//fun main() {
+//    val result = higherOrder(addition)
+//    println("Result: $result")
+//}
+
+//Example for higher order function
+
+fun addition(a: Int, b: Int): Int {
+    return a+b
 }
 
+fun higher() : ((Int,Int) -> Int) {
+    return ::addition
+}
+
+fun main() {
+    val multiply = higher()
+    val result = multiply(2,4)
+    println("The multiplication of two numbers is: $result")
+}
